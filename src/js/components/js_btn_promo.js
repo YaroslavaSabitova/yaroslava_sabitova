4e1ac6d8b541adc4a546b01c580424fde50ab255
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-06-23 22:43:26 +0300

    fixes everywhere

---
+++ src/js/components/js_btn_promo.js
@@ -0,0 +1,7 @@
+export default function () {
+    const btnPromo = document.querySelector('.js_btn_promo');
+
+    btnPromo.addEventListener('click', () => {
+        window.location.href = '#js_btn_promo';
+    });
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,11 @@
 // import js_burger from './components/js_burger.js';
-// import js_title from './components/js_title.js';
-// import js_shineElem from './components/js_shineElem.js';
 import js_clouds from './components/js_clouds.js';
 import js_changeBgColor from './components/js_changeBgColor.js';
+import js_btn_promo from './components/js_btn_promo.js';
 
 (function () {
     // js_burger();
-    // js_title();
-    // js_shineElem();
     js_clouds();
     js_changeBgColor();
+    js_btn_promo();
 })();
