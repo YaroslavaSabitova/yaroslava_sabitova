093f89e5259e487774cff92c30fa32384efc0a3f
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-06-28 12:33:05 +0300

    media and logo fixes

--- src/js/components/js_burger.js
+++ src/js/components/js_burger.js
@@ -5,11 +5,5 @@ export default function () {
     burgerBtn.addEventListener('click', () => {
         burgerBtn.classList.toggle('menu-open');
         menu.classList.toggle('menu-open');
-
-        if (menu.classList.contains('menu-open')) {
-            document.body.setAttribute('style', 'overflow: hidden');
-        } else {
-            document.body.removeAttribute('style');
-        }
     });
 }
