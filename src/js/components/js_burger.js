3421e54550439d1150e0486beb99dd6956cefb7a
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-07-17 15:49:45 +0300

    fix js mobile navigation

--- src/js/components/js_burger.js
+++ src/js/components/js_burger.js
@@ -2,8 +2,19 @@ export default function () {
     const menu = document.querySelector('.js_menu');
     const burgerBtn = document.querySelector('.js_burger');
 
+    const items = document.querySelectorAll('.js_item');
+
     burgerBtn.addEventListener('click', () => {
         burgerBtn.classList.toggle('menu-open');
         menu.classList.toggle('menu-open');
+    });
+
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            if (burgerBtn.classList.contains('menu-open')) {
+                menu.classList.remove('menu-open');
+                burgerBtn.classList.remove('menu-open');
+            }
+        });
     });
 }
